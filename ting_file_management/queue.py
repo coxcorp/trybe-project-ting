bf41bedba6ba70f1d5452ef6ceb3cfb72773b8b3
Diego Silva <coxcorp@gmail.com>
2022-08-07 11:07:29 -0300

    requisito 01

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,17 @@
 class Queue:
     def __init__(self):
-        pass
+        self._data = list()
 
     def __len__(self):
-        pass
+        return len(self._data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self._data.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if (index < 0):
+            raise IndexError()
+        return self._data[index]
