11f5ab39cdabdc479e2a947e4c4133b2e51c3702
Diego Silva <coxcorp@gmail.com>
2022-08-10 10:12:00 -0300

    requisito 03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,16 @@
+from .file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    data = txt_importer(path_file)
+    content = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(data),
+        "linhas_do_arquivo": data
+    }
+    if not len(instance):
+        instance.enqueue(content)
+        print(content)
 
 
 def remove(instance):
