7eadae9b341d8482176014617863c3cfeecca304
Diego Silva <coxcorp@gmail.com>
2022-08-10 18:28:43 -0300

    ajuste de escrita requisito 07

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,12 +1,12 @@
-def word_occurrence(word, instance, content=None):
+def word_occurrence(word, instance, complete=False):
     word = word.lower()
     word_occurrences = list()
     for index, item in enumerate(instance._data[0]["linhas_do_arquivo"]):
         if word in item.lower():
-            if content == "line":
-                word_occurrences.append({"linha": index + 1})
-            else:
+            if complete:
                 word_occurrences.append({"linha": index + 1, "conteudo": item})
+            else:
+                word_occurrences.append({"linha": index + 1})
     word_results = list()
     if word_occurrences:
         word_results = [
@@ -20,8 +20,8 @@ def word_occurrence(word, instance, content=None):
 
 
 def exists_word(word, instance):
-    return word_occurrence(word, instance, "line")
+    return word_occurrence(word, instance)
 
 
 def search_by_word(word, instance):
-    return word_occurrence(word, instance)
+    return word_occurrence(word, instance, True)
