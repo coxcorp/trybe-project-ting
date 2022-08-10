f2e4de3380dd7fb5eed920d137f1d876b3f276c4
Diego Silva <coxcorp@gmail.com>
2022-08-10 16:17:06 -0300

    requisito 06

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,18 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    word_occurrences = list()
+    for index, item in enumerate(instance._data[0]["linhas_do_arquivo"]):
+        if word.lower() in item.lower():
+            word_occurrences.append({"linha": index + 1})
+    word_results = list()
+    if word_occurrences:
+        word_results = [
+            {
+                "palavra": word,
+                "arquivo": instance._data[0]["nome_do_arquivo"],
+                "ocorrencias": word_occurrences,
+            }
+        ]
+    return word_results
 
 
 def search_by_word(word, instance):
